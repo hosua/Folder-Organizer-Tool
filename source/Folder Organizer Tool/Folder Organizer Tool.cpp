f0d0d990b0a5a6efcc1a72ae8803e9bb653a6554
hosua <hosua.o@gmail.com>
2021-05-25 06:54:40 -0400

    Build release

--- source/Folder Organizer Tool/Folder Organizer Tool.cpp
+++ source/Folder Organizer Tool/Folder Organizer Tool.cpp
@@ -7,12 +7,12 @@
 #include <filesystem>
 #include <fstream>
 #include <vector>
-#include <direct.h>	// For chdir
+#include <direct.h>	// For chdir // This does not work on linux.
 using std::filesystem::directory_iterator;
 
 std::filesystem::path getDir() {	// Ask user to enter a directory
 	std::string dirPath;
-	std::cout << "Enter the full directory path (right click to copy & paste)\n";	
+	std::cout << "Enter the full directory path (right click to paste from clipboard)\n";	
 	std::getline(std::cin, dirPath);	// This will std::cin the entire line instead ending at a space.
 	std::filesystem::path pathObj(dirPath);
 	if (!std::filesystem::exists(dirPath)) {
@@ -248,13 +248,3 @@ int main()
 	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
